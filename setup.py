d9e239e2ad4739c536cf6fde318bd1329414df51
klivolks <klivolks@gmail.com>
2023-05-26 01:31:37 +0100

    v0.0.1 - Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='KlivolksPDF',
+    version='0.0.1',
+    description='Generate PDF Documents for seamless operation of invoices, receipts and documents.',
+    author='Vishnu Prakash',
+    author_email='vishnu@klivolks.com',
+    url='https://github.com/klivolks/KlivolksPDF',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11'
+    ],
+)
